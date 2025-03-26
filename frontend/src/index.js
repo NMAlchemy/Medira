edc776dd345bd5f7c48e4214cce42bfb895acb16
NMAlchemy <91698193+NMAlchemy@users.noreply.github.com>
2025-03-26 17:29:05 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
